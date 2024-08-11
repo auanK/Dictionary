af8bd2792e6756938647a1807848ddee00254396
auanK <kauanpablo089@gmail.com>
2024-08-11 18:01:01 -0300

    Add ICU library for improved UTF-8 string handling S

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,33 +1,44 @@
 #include <fstream>
 #include <iostream>
-#include <locale>
-#include <sstream>
+#include <unicode/unistr.h>
+#include <unicode/ustream.h>
+#include <unicode/ucnv.h>
+#include <unicode/uchar.h>
+#include <string>
+#include <sstream> 
+
 
 using namespace std;
+using namespace icu;
 
-// Retorna o conteúdo de um arquivo em um stringstream formatado.
-stringstream read_file(const string& file_name) {
-    // Abre o arquivo e verifica se foi aberto corretamente
-    ifstream file(file_name);
-
-    if (!file.is_open()) {
-        cerr << "Error: Could not open file " << file_name << endl;
+// Retorna uma stringstream com as palavras de um arquivo de texto formatadas
+stringstream read_file(const string &file_path) {
+    // Abre o arquivo
+    ifstream input_file(file_path, ios::binary);
+    if (!input_file.is_open()) {
+        cerr << "Error: Could not open file " << file_path << endl;
         exit(1);
     }
+    
+    // Salva o conteúdo do arquivo em uma string e converte para UnicodeString
+    string file_content((istreambuf_iterator<char>(input_file)), istreambuf_iterator<char>());
+    UnicodeString unicode_content = UnicodeString::fromUTF8(StringPiece(file_content));
 
-    // Itera sobre o conteúdo do arquivo e armazena em uma string
-    string content((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
-
-    // Remove sinais de pontuação e converte todas as letras para minúsculas
-    // Observações:
-    // Falta fazer a conversão para minúsculas de letras acentuadas
-    // e falta permitir o uso de hífen em palavras compostas.
-    for (int i = 0; i < content.size(); i++) {
-        content[i] = towlower(content[i]);
-        if (ispunct(content[i])) {
-            content[i] = ' ';
+    // Converte todos os caracteres para minúsculo e substitui pontuações por espaços
+    // Obs: Falta permitir o uso de hífen em palavras compostas
+    unicode_content.toLower();
+    for (int i = 0; i < unicode_content.length(); i++) {
+        UChar character = unicode_content[i];
+        if (u_charType(character) == U_DASH_PUNCTUATION || u_charType(character) == U_OTHER_PUNCTUATION) {
+            unicode_content.replace(i, 1, ' ');
         }
     }
 
-    return stringstream(content);
+    // Converte UnicodeString para string com o texto formatado
+    string processed_content;
+    unicode_content.toUTF8String(processed_content);
+
+    // Retorna um stringstream com o conteúdo do arquivo
+    return stringstream(processed_content);
+    
 }
