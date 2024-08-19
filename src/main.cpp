88d9acf3d6b7786ec9adab4323de392aa63cb5ef
auanK <kauanpablo089@gmail.com>
2024-08-19 11:55:11 -0300

    Add argument usage to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
 #include "../include/avl_tree/avl_tree.hpp"
 #include "../include/dictionary.hpp"
@@ -7,16 +9,29 @@
 using namespace std;
 using namespace icu;
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cerr << "Uso: " << argv[0] << " <modo_estrutura> <arquivo>" << endl;
+        return 1;
+    }
+
+    string mode_structure = argv[1];
+    string filename = argv[2];
+
+    if (mode_structure != "dictionary_avl") {
+        cerr << "Estrutura não suportada: " << mode_structure << endl;
+        return 1;
+    }
+
     dictionary<avl_tree<UnicodeString, unicode_compare>> dict;
 
-    stringstream file = read_file("in/memorias_postumas_de_braz_cubas.txt");
+    stringstream file = read_file(filename);
     string word;
     while (file >> word) {
         dict.insert(UnicodeString::fromUTF8(StringPiece(word)));
     }
 
-    cout << "Dicionário de palavras:\n";
+    cout << "Dicionário de palavras:" << endl;
     string list;
     UnicodeString words = dict.list();
     words.toUTF8String(list);
